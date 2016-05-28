bb13b0023c08d2b9aa548782c3d1bfcf4281d134
gonzalojmartin <gonzalojmartin@hotmail.com>
2016-05-28 19:48:45 -0300

    getstatus WS ok

--- app/src/main/java/com/example/gmartin/alarmaiot_soa/MainActivity.java
+++ app/src/main/java/com/example/gmartin/alarmaiot_soa/MainActivity.java
@@ -6,6 +6,11 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.URL;
+
 public class MainActivity extends AppCompatActivity {
 
     @Override
@@ -13,23 +18,36 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        initNavigation();
+        getAlarmStatusFromWS();
+    }
+
+    private void getAlarmStatusFromWS() {
+        new NetworkTask(new Callback() {
+            @Override
+            public void run(String result) {
+                System.out.println("- ESTADO ALARMA :" + String.valueOf(result));
+            }
+        }).execute("http://192.168.1.72:8080/app/rest/alarm/status");
+    }
+
+    private void initNavigation() {
         Button btnConfig = (Button)findViewById(R.id.btn_config);
         Button btnStatus = (Button)findViewById(R.id.btn_status);
 
         btnConfig.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                                           // Source ---------> Destiny
-                Intent intent = new Intent(MainActivity.this, ConfigActivity.class);
-                startActivity(intent);
+            Intent intent = new Intent(MainActivity.this, ConfigActivity.class);
+            startActivity(intent);
             }
         });
 
         btnStatus.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Intent intent = new Intent(MainActivity.this, StatusActivity.class);
-                startActivity(intent);
+            Intent intent = new Intent(MainActivity.this, StatusActivity.class);
+            startActivity(intent);
             }
         });
     }
---
+++ app/src/main/java/com/example/gmartin/alarmaiot_soa/NetworkTask.java
@@ -0,0 +1,47 @@
+package com.example.gmartin.alarmaiot_soa;
+
+import android.os.AsyncTask;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.URL;
+
+/**
+ * Created by gmartin on 28/05/2016.
+ */
+public class NetworkTask extends AsyncTask<String, Void, String> {
+
+    private Callback callback;
+
+    public NetworkTask(Callback callback) {
+        this.callback = callback;
+    }
+
+    @Override
+    protected String doInBackground(String... params) {
+        try {
+            URL url = new URL(params[0]);
+            HttpURLConnection connection = (HttpURLConnection) url.openConnection();
+            connection.setRequestMethod("GET");
+
+            BufferedReader in = new BufferedReader(new InputStreamReader(connection.getInputStream()));
+            String inputLine;
+            StringBuffer response = new StringBuffer();
+            while((inputLine = in.readLine()) != null) {
+                response.append(inputLine);
+            }
+            return response.toString();
+
+        } catch (Exception ex) {
+            ex.printStackTrace();
+            return null;
+        }
+    }
+
+    @Override
+    protected void onPostExecute(String s) {
+        if (callback != null)
+            callback.run(s);
+    }
+}
