65ff4a5a63a0dfa7c250e360b375294df8b026df
gonzalojmartin <gonzalojmartin@hotmail.com>
2016-05-28 20:45:20 -0300

    alarma beta ok

--- app/src/main/java/com/example/gmartin/alarmaiot_soa/MainActivity.java
+++ app/src/main/java/com/example/gmartin/alarmaiot_soa/MainActivity.java
@@ -7,37 +7,82 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.CompoundButton;
 import android.widget.Switch;
+import android.widget.TextView;
+
+import com.google.gson.Gson;
+
+import java.util.Timer;
+import java.util.TimerTask;
 
 public class MainActivity extends AppCompatActivity {
+    private boolean status;
     private Switch switchOnOff;
     private Button btnConfig;
-    private Button btnStatus;
+    private TextView textTemperature;
+    private Timer timer;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        timer = new Timer();
 
-        switchOnOff = (Switch) findViewById(R.id.switch_on_off);
         btnConfig = (Button) findViewById(R.id.btn_config);
-        btnStatus = (Button) findViewById(R.id.btn_status);
-
+        textTemperature = (TextView) findViewById(R.id.text_temperature);
+        switchOnOff = (Switch) findViewById(R.id.switch_on_off);
         switchOnOff.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
             @Override
-            public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
-                new NetworkTask().execute("http://192.168.1.72:8080/app/rest/alarm/" + (isChecked == true ? "on" : "off"));
+            public void onCheckedChanged(CompoundButton buttonView, final boolean isChecked) {
+                new NetworkTask(new Callback() {
+                    @Override
+                    public void run(String result) {
+                        status = isChecked;
+                        initTemperature(isChecked);
+                    }
+                }).execute("http://192.168.1.72:8080/app/rest/alarm/" + (isChecked == true ? "on" : "off"));
             }
         });
 
         initNavigation();
         initAlarmStatusFromWS();
+
+
+        TimerTask timerTask = new TimerTask(){
+            @Override
+            public void run() {
+                if (status) {
+                    getTemperatureValueFromWS();
+                }
+            }
+        };
+        timer.schedule(timerTask, 0, 1500);
+    }
+
+    private void initTemperature(boolean alarmOn) {
+        if(!alarmOn){
+            textTemperature.setText("-- º C");
+        } else {
+            getTemperatureValueFromWS();
+        }
+    }
+
+    private void getTemperatureValueFromWS() {
+        new NetworkTask(new Callback() {
+            @Override
+            public void run(String result) {
+                Double temperature = Double.valueOf(result.toString());
+                textTemperature.setText(Math.round(temperature*100)/100 + "º C");
+            }
+        }).execute("http://192.168.1.72:8080/app/rest/temperature/read");
     }
 
     private void initAlarmStatusFromWS() {
         new NetworkTask(new Callback() {
             @Override
             public void run(String result) {
-                switchOnOff.setChecked(Boolean.valueOf(result));
+                status = Boolean.valueOf(result);
+                switchOnOff.setChecked(status);
+                initTemperature(status);
             }
         }).execute("http://192.168.1.72:8080/app/rest/alarm/status");
     }
@@ -47,14 +92,6 @@ public class MainActivity extends AppCompatActivity {
             @Override
             public void onClick(View view) {
             Intent intent = new Intent(MainActivity.this, ConfigActivity.class);
-            startActivity(intent);
-            }
-        });
-
-        btnStatus.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-            Intent intent = new Intent(MainActivity.this, StatusActivity.class);
             startActivity(intent);
             }
         });
