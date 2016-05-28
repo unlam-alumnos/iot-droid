1928a896fc7a21a77c037dd48fb9a2c3477b711d
gonzalojmartin <gonzalojmartin@hotmail.com>
2016-05-28 18:10:06 -0300

    Navegacion entre pantallas

--- app/src/main/java/com/example/gmartin/alarmaiot_soa/MainActivity.java
+++ app/src/main/java/com/example/gmartin/alarmaiot_soa/MainActivity.java
@@ -14,12 +14,21 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         Button btnConfig = (Button)findViewById(R.id.btn_config);
+        Button btnStatus = (Button)findViewById(R.id.btn_status);
 
         btnConfig.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                                            // Source ---------> Destiny
                 Intent intent = new Intent(MainActivity.this, ConfigActivity.class);
+                startActivity(intent);
+            }
+        });
+
+        btnStatus.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(MainActivity.this, StatusActivity.class);
                 startActivity(intent);
             }
         });
