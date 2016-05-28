0bf81b2785a86a213412a9bf650f045191e2a5e7
gonzalojmartin <gonzalojmartin@hotmail.com>
2016-05-28 17:48:25 -0300

    Pantalla principal y de configuracion

---
+++ app/src/main/java/com/example/gmartin/alarmaiot_soa/MainActivity.java
@@ -0,0 +1,27 @@
+package com.example.gmartin.alarmaiot_soa;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        Button btnConfig = (Button)findViewById(R.id.btn_config);
+
+        btnConfig.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                                           // Source ---------> Destiny
+                Intent intent = new Intent(MainActivity.this, ConfigActivity.class);
+                startActivity(intent);
+            }
+        });
+    }
+}
