014751d841067574149fad9a7f803dc2ee717469
gonzalojmartin <gonzalojmartin@hotmail.com>
2016-06-01 15:52:09 -0300

    Rename method to get limits

--- app/src/main/java/com/example/gmartin/alarmaiot_soa/ConfigActivity.java
+++ app/src/main/java/com/example/gmartin/alarmaiot_soa/ConfigActivity.java
@@ -5,7 +5,6 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
-import android.widget.NumberPicker;
 
 import com.google.gson.Gson;
 
@@ -31,12 +30,12 @@ public class ConfigActivity extends AppCompatActivity {
             }
         });
 
-        initConfigValuesFromWS();
+        getConfigValuesFromWS();
 
     }
 
 
-    private void initConfigValuesFromWS() {
+    private void getConfigValuesFromWS() {
         new NetworkTask(new Callback() {
             @Override
             public void run(String result) {
